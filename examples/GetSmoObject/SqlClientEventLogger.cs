54e95cd095e232e43e711e65a83d55514ddc5e0e
David Shiflet <david.shiflet@microsoft.com>
2023-04-04 11:36:42 -0500

    Example app for GetSmoObject (#133)
    * Example app for GetSmoObject
    
    * add readme

---
+++ examples/GetSmoObject/SqlClientEventLogger.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+
+namespace GetSmoObject
+{
+    internal class SqlClientEventLogger : EventListener
+    {
+        private StringBuilder logs= new StringBuilder();
+
+        public SqlClientEventLogger()
+        {
+        }
+
+        protected override void OnEventSourceCreated(EventSource eventSource)
+        {
+            if (eventSource.Name.Equals("Microsoft.Data.SqlClient.EventSource"))
+            {
+                EnableEvents(eventSource, EventLevel.Verbose, (EventKeywords)3);
+            }
+        }
+
+        protected override void OnEventWritten(EventWrittenEventArgs eventData)
+        {
+            if (eventData.Payload != null)
+            {
+                var now = DateTime.UtcNow.ToString("o");
+                logs = logs.Append(string.Join(Environment.NewLine, eventData.Payload.Select(p => $"{now} [{Environment.CurrentManagedThreadId}]: {p}")) + Environment.NewLine);
+            }
+        }
+
+        public override string ToString() => logs.ToString();
+    }
+}
